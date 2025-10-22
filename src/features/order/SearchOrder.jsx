f6995393419659a2aa6e83fd499681951f028e42
Rahul-jfs <ra1290hul@gmail.com>
2025-10-22 15:06:40 +0530

    Pizza app

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full px-4 py-2 bg-yellow-100 placeholder:text-stone-400 sm:focus:w-72 sm:w-64 transition-all focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-offset-2 "
+      />
+    </form>
+  );
+}
